75bc6d0088f166df276e37a4e77ccaee8a58f75a
Rich Greenhill <rgnet1@yahoo.com>
2016-04-11 11:12:55 -0700

    fix cityName on api endpoint

--- src/containers/search_bar.jsx
+++ src/containers/search_bar.jsx
@@ -18,7 +18,7 @@ class SearchBar extends Component {
   
   onFormSubmit(event) {
     event.preventDefault();
-    this.props.fetchWeather(this.state.term);
+    this.props.fetchWeather(this.state.searchTerm);
     this.setState({ searchTerm: '' });
   }
   

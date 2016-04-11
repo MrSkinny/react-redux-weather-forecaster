3fa59df9570329d81613c4f907b982dec9cca23e
Rich Greenhill <rgnet1@yahoo.com>
2016-04-11 12:03:51 -0700

    reducer calls action, concats old state onto new state and returns it

--- src/reducers/reducer_weather.jsx
+++ src/reducers/reducer_weather.jsx
@@ -1,4 +1,11 @@
-export default function(state = null, action) {
-  console.log('Action received: ', action);
+import { FETCH_WEATHER } from '../actions/index';
+
+export default function(state = [], action) {
+  switch(action.type){
+    case FETCH_WEATHER:
+      // do not to an Array.push -- must not mutate state in reducer, must return NEW instance of state
+      // below is ES6 sugar -- the ...state adds former array items and flattens
+      return [ action.payload.data, ...state ];
+  }
   return state;
 }

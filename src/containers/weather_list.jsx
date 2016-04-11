33228e49173af77f3a452d960790592fedd6602b
Rich Greenhill <rgnet1@yahoo.com>
2016-04-11 12:14:29 -0700

    update weatherList to container, hook to react-redux connect; create mapStateToProps

--- src/containers/weather_list.jsx
+++ src/containers/weather_list.jsx
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
-export default class WeatherList extends Component {
+class WeatherList extends Component {
   render(){
     return (
       <table className="table table-hover">
         <thead>
         <tr>
           <th>City</th>
+          <th>Temperature</th>
+          <th>Pressure</th>
+          <th>Humidity</th>
         </tr>
         </thead>
         <tbody>
@@ -15,3 +19,11 @@ export default class WeatherList extends Component {
     );
   }
 }
+
+// ES6 sugar: ({ weather }) is from `state` and pulls `state.weather`
+// ES6 sugar: `return { weather };` is return { weather: weather }
+function mapStateToProps({weather}){
+  return { weather };
+}
+
+export default connect(mapStateToProps)(WeatherList);
